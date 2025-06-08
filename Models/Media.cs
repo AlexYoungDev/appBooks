5f0f5b87580cf6fb9ce35290162e3c6aa7dec688
uvsq22101020 <hadam92@outlook.fr>
2025-06-08 12:20:57 +0200

    modif value nullable

--- Models/Media.cs
+++ Models/Media.cs
@@ -1,8 +1,8 @@
 public abstract class Media : IReadable
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
 
     public abstract void DisplayInformation();
 }
